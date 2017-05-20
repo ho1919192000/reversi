bc392adfb1ce9564d8d4e86be136f540a05f048c
Howard Huang <ho1919192000@gmail.com>
2017-05-20 00:35:13 -0600

    Change "" into ''

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 /*Include the static file webserver library*/
-var static = require("node-static");
+var static = require('node-static');
 /*Include the http server library*/
-var http = require("http");
+var http = require('http');
 /*Assume that we are running on Heroku*/
 var port = process.env.PORT;
-var directory = __dirname + "public";
+var directory = __dirname + 'public';
 
 /*If we aren't on Heroku, then we need to read just the port and directory information and we know that because port won't be set*/
-if (typeof port == "undefined" || !port) {
-    directory = "./public";
+if (typeof port == 'undefined' || !port) {
+    directory = './public';
     port = 8080;
 }
 
@@ -18,11 +18,11 @@ var file = new static.Server(directory);
 /*Construct an http server that gets files from the file server*/
 var app = http.createServer(
     function (request, response) {
-        request.addListener("end", function () {
+        request.addListener('end', function () {
             file.serve(request, response);
         }
                            ).resume();
     }
 ).listen(port);
 
-console.log("The Server is running");
+console.log('The Server is running');
